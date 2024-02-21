cebe66f19b918685c51eab1e663ad234ffcf0647
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-02-21 11:25:17 -0300

    Mudança na lógica e Adição de todas aulas do 1 semestre

--- src/App.jsx
+++ src/App.jsx
@@ -9,179 +9,151 @@ function App() {
   
   const diaDaSemanaValor = new Date().getDay();
 
+  const [disciplinas, setDisciplinas] = useState([
+    {
+      id: 0,
+      nome: '------',
+      professor: '------',
+      lugar: '------',
+      descricao: '------'
+    },
+    {
+      id: 1,
+      nome: 'MAT222',
+      professor: 'Antonio Magno',
+      lugar: 'BLOCO O - Sala 7',
+      descricao: 'Matemática I',
+    },
+    {
+      id: 2,
+      nome: 'LET100',
+      professor: 'Wesley Correia',
+      lugar: 'BLOCO B - Sala 6',
+      descricao: 'Língua Portuguesa',
+    },
+    [
+      {
+        id: 3.0,
+        nome: 'LET102 T01',
+        professor: 'Luzia Helena',
+        lugar: 'Bloco L - Cen. de Idiomas',
+        descricao: 'Inglês',
+      },
+      {
+        id: 3.1,
+        nome: 'LET102 T02',
+        professor: 'Luzia Helena',
+        lugar: 'Bloco L - Cen. de Idiomas',
+        descricao: 'Inglês',
+      },
+    ],
+    [
+      {
+        id: 4.0,
+        nome: 'INF027 T01',
+        professor: 'Fred',
+        lugar: 'BLOCO E - Lab. 3',
+        descricao: 'Introdução a Lógica de Programação',
+      },
+      {
+        id: 4.1,
+        nome: 'INF027 T02',
+        professor: 'Romilson',
+        lugar: 'BLOCO E - Lab. 4',
+        descricao: 'Introdução a Lógica de Programação',
+      },
+    ],
+    [
+      {
+        id: 5.0,
+        nome: 'INF026',
+        professor: 'Antônio Carlos (AC)',
+        lugar: 'BLOCO L - Sala 1',
+        descricao: 'Introdução a Computação',
+      },
+      {
+        id: 5.1,
+        nome: 'INF026',
+        professor: 'Antônio Carlos (AC)',
+        lugar: 'BLOCO E - Lab. 3',
+        descricao: 'Introdução a Computação',
+      }
+    ],
+  ])
+
   const [aulas, setAulas] = useState([
     {
       id: 0,
       dia: 'Domingo',
       aulas: [
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        }
+        disciplinas[0],
+        disciplinas[0],
+        disciplinas[0]
       ]
     },
     {
       id: 1,
       dia: 'Segunda-Feira',
       aulas: [
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: 'MAT222',
-          professor: 'Antonio Magno',
-          lugar: 'BLOCO O - Sala 7',
-          descricao: 'Matemática I',
-        },
-        {
-          nome: 'LET100',
-          professor: 'Wesley Correia',
-          lugar: 'BLOCO B - Sala 6',
-          descricao: 'Língua Portuguesa',
-        }
+        disciplinas[0],
+        disciplinas[1],
+        disciplinas[2]
       ]
     },
     {
       id: 2,
       dia: 'Terça-Feira',
       aulas: [
-        {
-          nome: 'LET102',
-          professor: 'Luzia Helena',
-          lugar: 'Bloco L - Cen. de Idiomas',
-          descricao: 'Inglês',
-        },
-        {
-          nome: 'INF027 T02',
-          professor: 'Romilson',
-          lugar: 'BLOCO E - Lab. 4',
-          descricao: 'Introdução a Lógica de Programação',
-        },
-        {
-          nome: 'INF026',
-          professor: 'Antônio Carlos (AC)',
-          lugar: 'BLOCO L - Sala 1',
-          descricao: 'Introdução a Computação',
-        }
+        disciplinas[3][1],
+        [
+          disciplinas[4][0],
+          disciplinas[4][1]
+        ],
+        disciplinas[5][0]
       ]
     },
     {
       id: 3,
       dia: 'Quarta-Feira',
       aulas: [
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: 'MAT222',
-          professor: 'Antonio Magno',
-          lugar: 'BLOCO O - Sala 7',
-          descricao: 'Matemática I',
-        },
-        {
-          nome: 'INF027 T02',
-          professor: 'Romilson',
-          lugar: 'BLOCO E - Lab. 4',
-          descricao: 'Introdução a Lógica de Programação',
-        }
+        disciplinas[3][0],
+        disciplinas[1],
+        disciplinas[4][1]
       ]
     },
     {
       id: 4,
       dia: 'Quinta-Feira',
       aulas: [
-        {
-          nome: 'LET100',
-          professor: 'Wesley Correia',
-          lugar: 'BLOCO B - Sala 6',
-          descricao: 'Língua Portuguesa',
-        },
-        {
-          nome: 'INF026',
-          professor: 'Antônio Carlos (AC)',
-          lugar: 'BLOCO E - Lab. 3',
-          descricao: 'Introdução a Computação',
-        },
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        }
+        disciplinas[2],
+        disciplinas[5][1],
+        disciplinas[4][0]
       ]
     },
     {
       id: 5,
       dia: 'Sexta-Feira',
       aulas: [
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: 'MAT222',
-          professor: 'Antonio Magno',
-          lugar: 'BLOCO O - Sala 7',
-          descricao: 'Matemática I',
-        },
-        {
-          nome: 'LET102',
-          professor: 'Luzia Helena',
-          lugar: 'Bloco L - Cen. de Idiomas',
-          descricao: 'Inglês',
-        }
+        disciplinas[3][0],
+        disciplinas[1],
+        disciplinas[3][1]
       ]
     },
     {
       id: 6,
       dia: 'Sábado',
       aulas: [
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        },
-        {
-          nome: '------',
-          professor: '------',
-          lugar: '------',
-          descricao: '------',
-        }
+        disciplinas[0],
+        disciplinas[0],
+        disciplinas[0]
       ]
     },
   ])
   return (
     <>
-    <Header/>
+    <Header
+      
+    />
     <DiaDaSemana 
       diaDaSemana={aulas[diaDaSemanaValor].dia}
       infoDia={aulas[diaDaSemanaValor].aulas}
--- src/components/diaDaSemana.jsx
+++ src/components/diaDaSemana.jsx
@@ -8,56 +8,104 @@ export default function DiaDaSemana({diaDaSemana, infoDia}) {
             <article>
                 <section className="primeiro">
                     <div>
-                        <h3>
+                        <h3 className="horarios">
                             17h 18h40
                         </h3>
                     </div>
-                    <div>
-                        <h4>
-                            {infoDia[0].nome} - {infoDia[0].professor}
-                        </h4>
-                        <h4>
-                            {infoDia[0].lugar}
-                        </h4>
-                        <h5>
-                            {infoDia[0].descricao}
-                        </h5>
+                    <div className="blocos-container">
+                        {infoDia[0][0] ? infoDia[0].map((e) => (
+                            <div className="blocos">
+                                <h4 className="titulo-aula">
+                                    {e.nome} - {e.professor}
+                                </h4>
+                                <h4 className="lugar-aula">
+                                    {e.lugar}
+                                </h4>
+                                <h5 className="info-aula">
+                                    {e.descricao}
+                                </h5>
+                            </div>
+                        )) : 
+                        <div className="blocos">
+                            <h4 className="titulo-aula">
+                                {infoDia[0].nome} - {infoDia[0].professor}
+                            </h4>
+                            <h4 className="lugar-aula">
+                                {infoDia[0].lugar}
+                            </h4>
+                            <h5 className="info-aula">
+                                {infoDia[0].descricao}
+                            </h5>
+                        </div>  
+                        }
                     </div>
                 </section>
                 <section className="segundo">
-                    <div>
+                    <div className="horarios">
                         <h3>
                             18h40 20h20
                         </h3>
                     </div>
-                    <div>
-                        <h4>
-                            {infoDia[1].nome} - {infoDia[1].professor}
-                        </h4>
-                        <h4>
-                            {infoDia[1].lugar}
-                        </h4>
-                        <h5>
-                            {infoDia[1].descricao}
-                        </h5>
+                    <div className="blocos-container">
+                        {infoDia[1][0] ? infoDia[1].map((e) => (
+                            <div className="blocos">
+                                <h4 className="titulo-aula">
+                                    {e.nome} - {e.professor}
+                                </h4>
+                                <h4 className="lugar-aula">
+                                    {e.lugar}
+                                </h4>
+                                <h5 className="info-aula">
+                                    {e.descricao}
+                                </h5>
+                            </div>
+                        )) : 
+                        <div className="blocos">
+                            <h4 className="titulo-aula">
+                                {infoDia[1].nome} - {infoDia[1].professor}
+                            </h4>
+                            <h4 className="lugar-aula">
+                                {infoDia[1].lugar}
+                            </h4>
+                            <h5 className="info-aula">
+                                {infoDia[1].descricao}
+                            </h5>
+                        </div>  
+                        }
                     </div>
                 </section>
                 <section className="terceiro">
                     <div>
-                        <h3>
+                        <h3 className="horarios">
                             20h20 22h
                         </h3>
                     </div>
-                    <div>
-                    <h4>
-                            {infoDia[2].nome} - {infoDia[2].professor}
-                        </h4>
-                        <h4>
-                            {infoDia[2].lugar}
-                        </h4>
-                        <h5>
-                            {infoDia[2].descricao}
-                        </h5>
+                    <div className="blocos-container">
+                        {infoDia[2][0] ? infoDia[2].map((e) => (
+                            <div className="blocos">
+                                <h4 className="titulo-aula">
+                                    {e.nome} - {e.professor}
+                                </h4>
+                                <h4 className="lugar-aula">
+                                    {e.lugar}
+                                </h4>
+                                <h5 className="info-aula">
+                                    {e.descricao}
+                                </h5>
+                            </div>
+                        )) : 
+                        <div className="blocos">
+                            <h4 className="titulo-aula">
+                                {infoDia[2].nome} - {infoDia[2].professor}
+                            </h4>
+                            <h4 className="lugar-aula">
+                                {infoDia[2].lugar}
+                            </h4>
+                            <h5 className="info-aula">
+                                {infoDia[2].descricao}
+                            </h5>
+                        </div>  
+                        }
                     </div>
                 </section>
             </article>
