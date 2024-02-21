c3b8041429ee40ad2df5a2b162362b0385feae47
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-02-21 00:37:12 -0300

    Mudança de lógica e adição de descrição

--- src/App.jsx
+++ src/App.jsx
@@ -8,57 +8,183 @@ import { useEffect, useState } from "react";
 function App() {
   
   const diaDaSemanaValor = new Date().getDay();
-  
-  const [diaDaSemana, setDiaDaSemana] = useState('nada')
-  
-  useEffect(() => {
-    switch (diaDaSemanaValor) {
-      case 0:
-        setDiaDaSemana("Domingo");
-        break;
-      case 1:
-        setDiaDaSemana("Segunda-Feira");
-        break;
-      case 2:
-        setDiaDaSemana("Terça-Feira");
-        break;
-      case 3:
-        setDiaDaSemana("Quarta-Feira");
-        break;
-      case 4:
-        setDiaDaSemana("Quinta-Feira");
-        break;
-      case 5:
-        setDiaDaSemana("Sexta-Feira");
-        break;
-      default:
-        setDiaDaSemana("Sábado")
-    }
-  })
-  //                          Domingo       Segunda      Terça       Quarta    Quinta    Sexta        Sábado
-  const primeiraAulaNome = ['não tem aula', '------', 'LET102'    , '------', 'LET100', '------', 'não tem aula']
-  const segundaAulaNome = ['não tem aula' , 'MAT222', 'INF027 T02', 'MAT222', 'INF026', 'MAT222', 'não tem aula']
-  const terceiraAulaNome = ['não tem aula', 'LET100', 'INF026'    , 'INF027', '------', 'LET102', 'não tem aula']
-  
-  //                        Domingo          Segunda               Terça                        Quarta                Quinta                     Sexta
-  const primeiraAulaInfo =  ['------', '------'          , 'BLOCO L-L - 002 - Cen. de Idiomas', '------'          , 'BLOCO B - Sala 6', '------']
-  const segundaAulaInfo =   ['------', 'BLOCO O - Sala 7', 'BLOCO E - Lab. 4'                 , 'BLOCO O - Sala 7', 'BLOCO B - Sala 2', 'BLOCO O - Sala 7']
-  const terceiraAulaInfo =  ['------', 'BLOCO B - Sala 6', 'BLOCO L - Sala 1'                 , 'BLOCO E - Lab. 4', '------'          , 'BLOCO L-L - 002 - Cen. de Idiomas']
-   
+
+  const [aulas, setAulas] = useState([
+    {
+      id: 0,
+      dia: 'Domingo',
+      aulas: [
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        }
+      ]
+    },
+    {
+      id: 1,
+      dia: 'Segunda-Feira',
+      aulas: [
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: 'MAT222',
+          professor: 'Antonio Magno',
+          lugar: 'BLOCO O - Sala 7',
+          descricao: 'Matemática I',
+        },
+        {
+          nome: 'LET100',
+          professor: 'Wesley Correia',
+          lugar: 'BLOCO B - Sala 6',
+          descricao: 'Língua Portuguesa',
+        }
+      ]
+    },
+    {
+      id: 2,
+      dia: 'Terça-Feira',
+      aulas: [
+        {
+          nome: 'LET102',
+          professor: 'Luzia Helena',
+          lugar: 'Bloco L - Cen. de Idiomas',
+          descricao: 'Inglês',
+        },
+        {
+          nome: 'INF027 T02',
+          professor: 'Romilson',
+          lugar: 'BLOCO E - Lab. 4',
+          descricao: 'Introdução a Lógica de Programação',
+        },
+        {
+          nome: 'INF026',
+          professor: 'Antônio Carlos (AC)',
+          lugar: 'BLOCO L - Sala 1',
+          descricao: 'Introdução a Computação',
+        }
+      ]
+    },
+    {
+      id: 3,
+      dia: 'Quarta-Feira',
+      aulas: [
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: 'MAT222',
+          professor: 'Antonio Magno',
+          lugar: 'BLOCO O - Sala 7',
+          descricao: 'Matemática I',
+        },
+        {
+          nome: 'INF027 T02',
+          professor: 'Romilson',
+          lugar: 'BLOCO E - Lab. 4',
+          descricao: 'Introdução a Lógica de Programação',
+        }
+      ]
+    },
+    {
+      id: 4,
+      dia: 'Quinta-Feira',
+      aulas: [
+        {
+          nome: 'LET100',
+          professor: 'Wesley Correia',
+          lugar: 'BLOCO B - Sala 6',
+          descricao: 'Língua Portuguesa',
+        },
+        {
+          nome: 'INF026',
+          professor: 'Antônio Carlos (AC)',
+          lugar: 'BLOCO E - Lab. 3',
+          descricao: 'Introdução a Computação',
+        },
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        }
+      ]
+    },
+    {
+      id: 5,
+      dia: 'Sexta-Feira',
+      aulas: [
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: 'MAT222',
+          professor: 'Antonio Magno',
+          lugar: 'BLOCO O - Sala 7',
+          descricao: 'Matemática I',
+        },
+        {
+          nome: 'LET102',
+          professor: 'Luzia Helena',
+          lugar: 'Bloco L - Cen. de Idiomas',
+          descricao: 'Inglês',
+        }
+      ]
+    },
+    {
+      id: 6,
+      dia: 'Sábado',
+      aulas: [
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        },
+        {
+          nome: '------',
+          professor: '------',
+          lugar: '------',
+          descricao: '------',
+        }
+      ]
+    },
+  ])
   return (
     <>
     <Header/>
     <DiaDaSemana 
-
-      diaDaSemana={diaDaSemana}
-
-      primeiraAulaNome={primeiraAulaNome[diaDaSemanaValor]}
-      segundaAulaNome={segundaAulaNome[diaDaSemanaValor]}
-      terceiraAulaNome={terceiraAulaNome[diaDaSemanaValor]}
-
-      primeiraAulaInfo={primeiraAulaInfo[diaDaSemanaValor]}
-      segundaAulaInfo={segundaAulaInfo[diaDaSemanaValor]}
-      terceiraAulaInfo={terceiraAulaInfo[diaDaSemanaValor]}
+      diaDaSemana={aulas[diaDaSemanaValor].dia}
+      infoDia={aulas[diaDaSemanaValor].aulas}
     />
     <Footer/>
     </>
--- src/components/diaDaSemana.jsx
+++ src/components/diaDaSemana.jsx
@@ -1,4 +1,4 @@
-export default function DiaDaSemana({diaDaSemana, primeiraAulaNome, primeiraAulaInfo, segundaAulaNome, segundaAulaInfo, terceiraAulaNome, terceiraAulaInfo}) {
+export default function DiaDaSemana({diaDaSemana, infoDia}) {
 
     return (
         <main>
@@ -14,11 +14,14 @@ export default function DiaDaSemana({diaDaSemana, primeiraAulaNome, primeiraAula
                     </div>
                     <div>
                         <h4>
-                            {primeiraAulaNome}
+                            {infoDia[0].nome} - {infoDia[0].professor}
                         </h4>
                         <h4>
-                            {primeiraAulaInfo}
+                            {infoDia[0].lugar}
                         </h4>
+                        <h5>
+                            {infoDia[0].descricao}
+                        </h5>
                     </div>
                 </section>
                 <section className="segundo">
@@ -29,11 +32,14 @@ export default function DiaDaSemana({diaDaSemana, primeiraAulaNome, primeiraAula
                     </div>
                     <div>
                         <h4>
-                            {segundaAulaNome}
+                            {infoDia[1].nome} - {infoDia[1].professor}
                         </h4>
                         <h4>
-                            {segundaAulaInfo}
+                            {infoDia[1].lugar}
                         </h4>
+                        <h5>
+                            {infoDia[1].descricao}
+                        </h5>
                     </div>
                 </section>
                 <section className="terceiro">
@@ -43,12 +49,15 @@ export default function DiaDaSemana({diaDaSemana, primeiraAulaNome, primeiraAula
                         </h3>
                     </div>
                     <div>
-                        <h4>
-                            {terceiraAulaNome}
+                    <h4>
+                            {infoDia[2].nome} - {infoDia[2].professor}
                         </h4>
                         <h4>
-                            {terceiraAulaInfo}
+                            {infoDia[2].lugar}
                         </h4>
+                        <h5>
+                            {infoDia[2].descricao}
+                        </h5>
                     </div>
                 </section>
             </article>
