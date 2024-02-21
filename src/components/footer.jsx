a7dbe69fd5872f3444259571ad6697efc45247fb
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-02-21 00:36:26 -0300

    alterações no texto do footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            Made By <a href="https://github.com/RafaelD-S">Rafael Dantas Silva</a>
+            Alpha V1 - Made By <a href="https://github.com/RafaelD-S">Rafael Dantas Silva</a>
         </footer>
     )
 }
