408d5efc960d0b59043b966fee02a951db2c163a
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-02-22 14:06:53 -0300

    mudancas

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            Alpha V2 - Made By <a href="https://github.com/RafaelD-S">Rafael Dantas Silva</a>
+            Alpha V2 - Made By <a href="https://github.com/RafaelD-S" target="blank_">Rafael Dantas Silva</a>
         </footer>
     )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,35 @@
+import Sol from '../assets/sol.svg'
+import Calendario from '../assets/calendario.svg'
+import ArrowDown from '../assets/arrow-down.svg'
+import Reset from '../assets/recarregar.svg'
+
 export default function Header() {
     return (
         <header>
+            <section>
                 <h1>
                     Que Aula?
                 </h1>
+            </section>
+            {/* Implementação futura de:
+            - Ver todas as aulas
+            - Ver um calendário de aulas
+            - Mudar de tema escuro para tema claro */}
+
+            {/* <nav>
+                <ul>
+                    <li>Todas as Aulas <img src={ArrowDown} alt="" /></li>
+                    <li>
+                        <figure>
+                            <img src={Calendario} alt="Calendário" />
+                            <img src={Sol} alt="Mudar tema" />
+                        </figure>
+                    </li>
+                </ul>
+            </nav> */}
+            <figure className='reset' onClick={() => location.reload()}>
+                <img src={Reset} alt="reset" />
+            </figure>
         </header>
     )
 }
