622aa02baa4bd2fd57c269e26c66ebde442baf09
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-02-28 00:22:16 -0300

    Adcição de Botão de Reset

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,12 @@
+import Reset from '../assets/recarregar.svg'
+
 export default function Header() {
   return (
     <header>
       <h1>Que Aula?</h1>
+        <figure className='reset' onClick={() => location.reload()}>
+            <img src={Reset} alt="reset" />
+        </figure>
     </header>
   );
 }
