05ffcc2910bcdddd2384a9d7cad232907ed1907d
Rafael Dantas <113727583+RafaelD-S@users.noreply.github.com>
2024-09-07 22:19:24 -0300

    Delete src/components/Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      Alpha V2 - Made By{" "}
-      <a href="https://github.com/RafaelD-S">Rafael Dantas Silva</a>
-    </footer>
-  );
-}
