fc4303c82b78486005af677c1d2d83bbca0bb483
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-09-30 21:24:58 -0300

    feat: added new logic for class data consumption

--- src/App.jsx
+++ src/App.jsx
@@ -3,166 +3,28 @@ import Header from "./components/header/header.jsx";
 import Footer from "./components/footer/footer.jsx";
 
 import "./style/style.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./components/modal/modal.jsx";
 
 function App() {
   const [currentWeekday, setCurrentWeekday] = useState(new Date().getDay());
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const subjects = [
-    {
-      id: 0,
-      name: "------",
-      greve: false,
-      teacher: "------",
-      classroom: "------",
-      description: "------",
-    },
-    [
-      {
-        id: 1.0,
-        name: "MAT222",
-        greve: false,
-        teacher: "Jair Messias Matemática",
-        classroom: "BLOCO O - Sala 7",
-        description: "Matemática I",
-      },
-      {
-        id: 1.1,
-        name: "MAT222",
-        greve: false,
-        teacher: "Jair Messias Matemática",
-        classroom: "BLOCO D - Sala 4",
-        description: "Matemática I",
-      },
-    ],
-    [
-      {
-        id: 2.0,
-        name: "LET100",
-        greve: false,
-        teacher: "Wesley Correia",
-        classroom: "BLOCO B - Sala 6",
-        description: "Língua Portuguesa",
-      },
-      {
-        id: 2.1,
-        name: "LET100",
-        greve: false,
-        teacher: "Wesley Correia",
-        classroom: "BLOCO D - Sala 2",
-        description: "Língua Portuguesa",
-      },
-    ],
-    [
-      {
-        id: 3.0,
-        name: "LET102",
-        greve: false,
-        class: "T01",
-        teacher: "Luzia Helena",
-        classroom: "Bloco L - Cen. de Idiomas",
-        description: "Inglês Instrumental",
-      },
-      {
-        id: 3.1,
-        name: "LET102",
-        greve: false,
-        class: "T02",
-        teacher: "Luzia Helena",
-        classroom: "Bloco L - Cen. de Idiomas",
-        description: "Inglês Instrumental",
-      },
-    ],
-    [
-      {
-        id: 4.0,
-        name: "INF027",
-        greve: false,
-        class: "T01",
-        teacher: "Fred",
-        classroom: "BLOCO E - Lab. 3",
-        description: "Introdução a Lógica de Programação",
-      },
-      {
-        id: 4.1,
-        name: "INF027",
-        greve: false,
-        class: "T02",
-        teacher: "Romilson",
-        classroom: "BLOCO E - Lab. 4",
-        description: "Introdução a Lógica de Programação",
-      },
-    ],
-    [
-      {
-        id: 5.0,
-        name: "INF026",
-        greve: false,
-        teacher: "Antônio Carlos (AC)",
-        classroom: "BLOCO O - Sala 6",
-        description: "Introdução a Computação",
-      },
-      {
-        id: 5.1,
-        name: "INF026",
-        greve: false,
-        teacher: "Antônio Carlos (AC)",
-        classroom: "BLOCO E - Lab. 3",
-        description: "Introdução a Computação",
-      },
-    ],
-  ];
+  useEffect(() => {
+    !localStorage.getItem("chosenClasses") && setIsModalOpen(true);
+  }, []);
 
-  const schedule = [
-    {
-      id: 0,
-      day: "Domingo",
-      lectures: [[subjects[0]], [subjects[0]], [subjects[0]]],
-    },
-    {
-      id: 1,
-      day: "Segunda-Feira",
-      lectures: [[subjects[0]], [subjects[1][0]], [subjects[2][0]]],
-    },
-    {
-      id: 2,
-      day: "Terça-Feira",
-      lectures: [
-        [subjects[3][1]],
-        [subjects[4][0], subjects[4][1]],
-        [subjects[5][0]],
-      ],
-    },
-    {
-      id: 3,
-      day: "Quarta-Feira",
-      lectures: [[subjects[3][0]], [subjects[1][0]], [subjects[4][1]]],
-    },
-    {
-      id: 4,
-      day: "Quinta-Feira",
-      lectures: [[subjects[2][1]], [subjects[5][1]], [subjects[4][0]]],
-    },
-    {
-      id: 5,
-      day: "Sexta-Feira",
-      lectures: [[subjects[3][0]], [subjects[1][1]], [subjects[3][1]]],
-    },
-    {
-      id: 6,
-      day: "Sábado",
-      lectures: [[subjects[0]], [subjects[0]], [subjects[0]]],
-    },
-  ];
+  const [selectedClasses, setSelectedClasses] = useState([]);
+
   return (
     <>
       <Header switchWeekday={setCurrentWeekday} />
-      <DayClasses
-        day={schedule[currentWeekday].day}
-        data={schedule[currentWeekday].lectures}
-      />
-      <Modal></Modal>
+      <DayClasses currentWeekday={currentWeekday} />
+      <Modal
+        setSelectedClasses={setSelectedClasses}
+        selectedClasses={selectedClasses}
+        isModalOpen={isModalOpen}
+      ></Modal>
       <Footer />
     </>
   );
--- src/components/dayClasses/dayClasses.jsx
+++ src/components/dayClasses/dayClasses.jsx
@@ -1,54 +1,231 @@
+import { useEffect, useState } from "react";
 import "./dayClassesStyle.scss";
 
-export default function dayClasses({ day, data }) {
-  const blocks = [
+export default function dayClasses({ currentWeekday }) {
+  const [selectedClasses, setSelectedClasses] = useState();
+  const [classes, setClasses] = useState([
     {
-      index: 0,
-      greve: data[0].greve,
-      time: "17h 18h40",
+      day: "Domingo",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
     },
     {
-      index: 1,
-      greve: data[1].greve,
-      time: "18h40 20h20",
+      day: "Segunda-Feira",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
     },
     {
-      index: 2,
-      greve: data[2].greve,
-      time: "20h20 22h",
+      day: "Terça-Feira",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
     },
-  ];
+    {
+      day: "Quarta-Feira",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
+    },
+    {
+      day: "Quinta-Feira",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
+    },
+    {
+      day: "Sexta-Feira",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
+    },
+    {
+      day: "Sábado",
+      classes: {
+        firstClass: [],
+        secClass: [],
+        thirdClass: [],
+      },
+    },
+  ]);
+
+  useEffect(() => {
+    const storedClasses = JSON.parse(
+      localStorage.getItem("chosenClasses") || "[]"
+    );
+    const selecionados = [];
+    storedClasses.forEach((e) => {
+      e.classes.forEach((f) => {
+        f.selected && !selecionados.includes(e) && selecionados.push(f);
+      });
+    });
+
+    setSelectedClasses(selecionados);
+    setClasses([
+      {
+        day: "Domingo",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 0 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 0 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 0 && f.period == 2
+          ),
+        },
+      },
+      {
+        day: "Segunda-Feira",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 1 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 1 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 1 && f.period == 2
+          ),
+        },
+      },
+      {
+        day: "Terça-Feira",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 2 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 2 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 2 && f.period == 2
+          ),
+        },
+      },
+      {
+        day: "Quarta-Feira",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 3 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 3 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 3 && f.period == 2
+          ),
+        },
+      },
+      {
+        day: "Quinta-Feira",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 4 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 4 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 4 && f.period == 2
+          ),
+        },
+      },
+      {
+        day: "Sexta-Feira",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 5 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 5 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 5 && f.period == 2
+          ),
+        },
+      },
+      {
+        day: "Sábado",
+        classes: {
+          firstClass: selecionados.filter(
+            (f) => f.weekDay == 6 && f.period == 0
+          ),
+          secClass: selecionados.filter((f) => f.weekDay == 6 && f.period == 1),
+          thirdClass: selecionados.filter(
+            (f) => f.weekDay == 6 && f.period == 2
+          ),
+        },
+      },
+    ]);
+  }, []);
 
   return (
     <main className="dayClasses">
-      <h2 className="dayClasses__title">{day}</h2>
+      <h2 className="dayClasses__title">{classes[currentWeekday].day}</h2>
       <article className="dayClasses__container">
-        {blocks.map((block) => (
-          <section key={block.index} className="dayClasses__item">
-            <div
-              className={`dayClasses__schedule${
-                block.greve ? "--greve" : "--normal"
-              }`}
-            >
-              <h3 className="dayClasses__schedule">{block.time}</h3>
-            </div>
-            <div className="dayClasses__info-container">
-              {data[block.index].map((e, index) => (
-                <div key={index} className="dayClasses__info-item">
-                  <h4 className="dayClasses__info-item-title">
-                    {e.name} <span>{e.class ? e.class : ""}</span> - {e.teacher}
-                  </h4>
-                  <h4 className="dayClasses__info-item-classroom">
-                    {e.classroom}
-                  </h4>
-                  <h5 className="dayClasses__info-item-description">
-                    {e.description}
-                  </h5>
-                </div>
-              ))}
-            </div>
-          </section>
-        ))}
+        <section className="dayClasses__item">
+          <div className={`dayClasses__schedule`}>
+            <h3 className="dayClasses__schedule">17h 18h40</h3>
+          </div>
+          <div className="dayClasses__info-container">
+            {classes[currentWeekday].classes.firstClass.map((e, index) => (
+              <div key={index} className="dayClasses__info-item">
+                <h4 className="dayClasses__info-item-title">
+                  {e.className} <span>{e.whichClass}</span> - {e.teacher}
+                </h4>
+                <h4 className="dayClasses__info-item-classroom">
+                  {e.classroom}
+                </h4>
+                <h5 className="dayClasses__info-item-description">
+                  {e.classDescription}
+                </h5>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className="dayClasses__item">
+          <div className={`dayClasses__schedule`}>
+            <h3 className="dayClasses__schedule">18h40 20h20</h3>
+          </div>
+          <div className="dayClasses__info-container">
+            {classes[currentWeekday].classes.secClass.map((e, index) => (
+              <div key={index} className="dayClasses__info-item">
+                <h4 className="dayClasses__info-item-title">
+                  {e.className} <span>{e.whichClass}</span> - {e.teacher}
+                </h4>
+                <h4 className="dayClasses__info-item-classroom">
+                  {e.classroom}
+                </h4>
+                <h5 className="dayClasses__info-item-description">
+                  {e.classDescription}
+                </h5>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className="dayClasses__item">
+          <div className={`dayClasses__schedule`}>
+            <h3 className="dayClasses__schedule">20h20 22h</h3>
+          </div>
+          <div className="dayClasses__info-container">
+            {classes[currentWeekday].classes.thirdClass.map((e, index) => (
+              <div key={index} className="dayClasses__info-item">
+                <h4 className="dayClasses__info-item-title">
+                  {e.className} <span>{e.whichClass}</span> - {e.teacher}
+                </h4>
+                <h4 className="dayClasses__info-item-classroom">
+                  {e.classroom}
+                </h4>
+                <h5 className="dayClasses__info-item-description">
+                  {e.classDescription}
+                </h5>
+              </div>
+            ))}
+          </div>
+        </section>
       </article>
     </main>
   );
