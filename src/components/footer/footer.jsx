347f8b613d651fde6f450d5e39364b4f626f728e
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-09-30 22:00:25 -0300

    feat: add erase calendar function

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,45 @@
 import "./footerStyle.scss";
+import closeIcon from "../../assets/close.svg";
+import { useState } from "react";
 
 export default function Footer() {
+  const [openWarning, setOpenWarning] = useState(false);
+
   return (
     <footer className="footer">
-      Alpha V3 - Made By{" "}
-      <a href="https://github.com/RafaelD-S" target="blank_">
-        Rafael Dantas Silva
+      <a className="footer__new-calendar" onClick={() => setOpenWarning(true)}>
+        Criar um novo calendário
       </a>
+      <div className="footer__credits">
+        Alpha V3 - Made By{" "}
+        <a href="https://github.com/RafaelD-S" target="blank_">
+          Rafael Dantas Silva
+        </a>
+      </div>
+      {openWarning && <Warning setOpenWarning={setOpenWarning} />}
     </footer>
+  );
+}
+
+export function Warning({ setOpenWarning }) {
+  const eraseCalendar = () => {
+    localStorage.clear();
+    location.reload();
+  };
+  return (
+    <div className="footer__warning" onClick={() => setOpenWarning(false)}>
+      <div className="footer__warning-content">
+        <img src={closeIcon} alt="" className="footer__warning-content__icon" />
+        <h2 className="footer__warning-content__title">
+          Você tem certeza que quer apagar seu calendário?
+        </h2>
+        <button
+          className="footer__warning-content__button"
+          onClick={eraseCalendar}
+        >
+          Sim
+        </button>
+      </div>
+    </div>
   );
 }
