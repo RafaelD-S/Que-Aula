55e5b8f898c635e2b48010e28f5aeda2fdaf5c0a
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2025-03-15 01:34:18 -0300

    feat: create new way to consume classes periods

--- src/components/dayClasses/dayClasses.tsx
+++ src/components/dayClasses/dayClasses.tsx
@@ -1,38 +1,38 @@
 import { useEffect, useState } from "react";
 import "./dayClassesStyle.scss";
-import { IClasses, IDayClasses } from "./dayClassesInterface";
+import { IClasses, IDayClasses, ISectionArray } from "./dayClassesInterface";
 import { IClassesData } from "../../types/dataClasses.interface";
 
 const DayClasses = ({ currentWeekday }: IDayClasses) => {
-  const classesTemplate = { firstClass: [], secClass: [], thirdClass: [] };
+  const [sections, setSections] = useState<ISectionArray[][]>([]);
   const [classes, setClasses] = useState<IClasses[]>([
     {
       day: "Domingo",
-      classes: classesTemplate,
+      classes: [],
     },
     {
       day: "Segunda-Feira",
-      classes: classesTemplate,
+      classes: [],
     },
     {
       day: "Terça-Feira",
-      classes: classesTemplate,
+      classes: [],
     },
     {
       day: "Quarta-Feira",
-      classes: classesTemplate,
+      classes: [],
     },
     {
       day: "Quinta-Feira",
-      classes: classesTemplate,
+      classes: [],
     },
     {
       day: "Sexta-Feira",
-      classes: classesTemplate,
+      classes: [],
     },
     {
       day: "Sábado",
-      classes: classesTemplate,
+      classes: [],
     },
   ]);
 
@@ -44,52 +44,124 @@ const DayClasses = ({ currentWeekday }: IDayClasses) => {
       prev.map((item, index) => {
         return {
           ...item,
-          classes: {
-            firstClass: specificClasses.filter((f) => +f.weekDay === index && +f.period === 0),
-            secClass: specificClasses.filter((f) => +f.weekDay === index && +f.period === 1),
-            thirdClass: specificClasses.filter((f) => +f.weekDay === index && +f.period === 2),
-          },
+          classes: specificClasses.filter((f) => +f.weekDay === index),
         };
       })
     );
   }, []);
 
-  const blocks = [
-    {
-      time: "17h 18h40",
-      data: classes[currentWeekday].classes.firstClass,
-    },
-    {
-      time: "18h40 20h20",
-      data: classes[currentWeekday].classes.secClass,
-    },
-    {
-      time: "20h20 22h",
-      data: classes[currentWeekday].classes.thirdClass,
-    },
-  ];
+  useEffect(() => {
+    const updatedSections = classes[currentWeekday].classes.map((item) => ({
+      start: item.period[0],
+      end: item.period[item.period.length - 1],
+      data: item,
+    }));
+    let newArray: ISectionArray[][] = Array(6).fill(null);
+
+    updatedSections.forEach((e) => {
+      for (let i = e.start; i <= e.end; i++) {
+        newArray[i] = newArray[i] ? [...newArray[i], e] : [e];
+      }
+    });
+
+    console.log(newArray);
+
+    let i = 0;
+    while (i < newArray.length) {
+      if (newArray[i] === null) {
+        let start = i;
+        let end = i;
+
+        while (end < newArray.length && newArray[end] === null) {
+          end++;
+        }
+
+        const emptySection = [
+          {
+            start: start,
+            end: end - 1,
+            data: null,
+          },
+        ];
+
+        for (let j = start; j < end; j++) {
+          newArray[j] = emptySection;
+        }
+
+        i = end;
+      } else {
+        i++;
+      }
+    }
+
+    const uniqueArray: ISectionArray[][] = [];
+    const seen = new Set();
+
+    newArray.forEach((item) => {
+      const key = `${item[0].start}-${item[0].end}-${item[0].data}`;
+
+      if (!seen.has(key)) {
+        seen.add(key);
+        uniqueArray.push(item);
+      }
+    });
+
+    setSections(uniqueArray);
+  }, [classes, currentWeekday]);
+
+  const definePeriod = (num: number) => {
+    switch (num) {
+      case 0:
+        return "17h";
+      case 1:
+        return "17h50";
+      case 2:
+        return "18h40";
+      case 3:
+        return "19h30";
+      case 4:
+        return "20h20";
+      case 5:
+        return "21h10";
+      case 6:
+        return "22h";
+    }
+  };
 
   return (
     <main className="dayClasses">
       <h2 className="dayClasses__title">{classes[currentWeekday].day}</h2>
       <article className="dayClasses__container">
-        {blocks.map((item, i) => (
-          <section className="dayClasses__item" key={i}>
+        {sections.map((item, i) => (
+          <section
+            className={`dayClasses__item ${!item[0].data ? "dayClasses__item--empty" : ""}`}
+            key={i}
+          >
             <div className={`dayClasses__schedule`}>
-              <h3 className="dayClasses__schedule">{item.time}</h3>
+              <h3 className="dayClasses__schedule">
+                {definePeriod(+item[0].start)} {definePeriod(+item[0].end + 1)}
+              </h3>
             </div>
             <div className="dayClasses__info-container">
-              {item.data.map((info, index) => (
-                <div key={index} className="dayClasses__info-item">
-                  <h4 className="dayClasses__info-item-title">
-                    {info.className}
-                    <span className="dayClasses__info-item-class"> {info.whichClass} </span>-{" "}
-                    {info.teacher}
-                  </h4>
-                  <h4 className="dayClasses__info-item-classroom">{info.classroom}</h4>
-                  <h5 className="dayClasses__info-item-description">{info.classDescription}</h5>
-                </div>
-              ))}
+              {item.map((info, index: number) =>
+                info.data ? (
+                  <div key={index} className="dayClasses__info-item">
+                    <h4 className="dayClasses__info-item-title">
+                      {info.data.className}
+                      <span className="dayClasses__info-item-class">
+                        {" "}
+                        {info.data.whichClass}{" "}
+                      </span>- {info.data.teacher}
+                    </h4>
+                    <h4 className="dayClasses__info-item-classroom">{info.data.classroom}</h4>
+                    <h5 className="dayClasses__info-item-description">
+                      {info.data.classDescription}
+                    </h5>
+                  </div>
+                ) : (
+                  <div className="dayClasses__info-item-title" key={index}></div>
+                )
+              )}
             </div>
           </section>
         ))}
--- src/components/dayClasses/dayClassesInterface.ts
+++ src/components/dayClasses/dayClassesInterface.ts
@@ -1,14 +1,24 @@
-import { IClassesData } from "../../types/dataClasses.interface";
-
 export interface IDayClasses {
   currentWeekday: number;
 }
 
 export interface IClasses {
   day: string;
-  classes: {
-    firstClass: IClassesData["classes"];
-    secClass: IClassesData["classes"];
-    thirdClass: IClassesData["classes"];
-  };
+  classes: any[];
+}
+
+export interface ISectionArray {
+  start: string | number;
+  end: string | number;
+  data: {
+    weekDay: string;
+    period: string[];
+    teacher: string;
+    selected?: boolean;
+    classList?: string[];
+    classroom: string;
+    whichClass?: string;
+    classDescription?: string;
+    className?: string;
+  } | null;
 }
