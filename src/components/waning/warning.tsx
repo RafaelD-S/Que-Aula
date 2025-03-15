fb7731a74d032897c028834c156e434c9076cb70
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2025-03-15 01:25:27 -0300

    feat: add new props to warning component

--- src/components/waning/warning.tsx
+++ src/components/waning/warning.tsx
@@ -1,15 +1,25 @@
 import { useRef, useState } from "react";
 import warning from "../../assets/warning.svg";
+import info from "../../assets/info.svg";
 import { IWarning } from "./warning.interface";
 import "./warningStyle.scss";
 
-const Warning = ({ children, message, buttonLabel, onClickButton, disabled = false }: IWarning) => {
-  const [isOpenState, setIsOpenState] = useState(false);
+const Warning = ({
+  children,
+  message,
+  buttonLabel,
+  onClickButton,
+  disabled = false,
+  opened = false,
+  isClosable = true,
+  type = "warning",
+}: IWarning) => {
+  const [isOpenState, setIsOpenState] = useState(opened);
   const modalRef = useRef<HTMLDivElement>(null);
 
   const handleOverlayClick = (e: React.SyntheticEvent) => {
     const target = e.target as HTMLDivElement;
-    if (modalRef.current && !modalRef.current.contains(target)) {
+    if (isClosable && modalRef.current && !modalRef.current.contains(target)) {
       setIsOpenState(false);
     }
   };
@@ -24,10 +34,19 @@ const Warning = ({ children, message, buttonLabel, onClickButton, disabled = fal
       {isOpenState && (
         <div className="warning" onClick={handleOverlayClick}>
           <div className="warning__content" ref={modalRef}>
-            <img src={warning} alt="" className="warning__content__icon" />
+            <img
+              src={type === "warning" ? warning : info}
+              alt=""
+              className="warning__content__icon"
+            />
             <h2 className="warning__content__title">{message}</h2>
             {buttonLabel && (
-              <button className="warning__content__button" onClick={onClickButton}>
+              <button
+                className={`warning__content__button warning__content__button${
+                  type === "warning" ? "--warning" : "--info"
+                }`}
+                onClick={onClickButton}
+              >
                 {buttonLabel}
               </button>
             )}
