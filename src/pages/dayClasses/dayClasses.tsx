beee38426e01d31460934dda47efb6a090be2e3d
Luana Lima <luanalima.2006@hotmail.com>
2025-03-22 00:34:29 -0300

    fix: remove duplicated className

--- src/pages/dayClasses/dayClasses.tsx
+++ src/pages/dayClasses/dayClasses.tsx
@@ -37,8 +37,12 @@ const DayClasses = ({ currentWeekday }: IDayClasses) => {
   ]);
 
   useEffect(() => {
-    const storedClasses: IClassesData[] = JSON.parse(localStorage.getItem("chosenClasses") || "[]");
-    const specificClasses = storedClasses.flatMap((item) => item.classes).filter((f) => f.selected);
+    const storedClasses: IClassesData[] = JSON.parse(
+      localStorage.getItem("chosenClasses") || "[]"
+    );
+    const specificClasses = storedClasses
+      .flatMap((item) => item.classes)
+      .filter((f) => f.selected);
 
     setClasses((prev) =>
       prev.map((item, index) => {
@@ -132,11 +136,13 @@ const DayClasses = ({ currentWeekday }: IDayClasses) => {
       <article className="dayClasses__container">
         {sections.map((item, i) => (
           <section
-            className={`dayClasses__item ${!item[0].data ? "dayClasses__item--empty" : ""}`}
+            className={`dayClasses__item ${
+              !item[0].data ? "dayClasses__item--empty" : ""
+            }`}
             key={i}
           >
             <div className={`dayClasses__schedule`}>
-              <h3 className="dayClasses__schedule">
+              <h3>
                 {definePeriod(+item[0].start)} {definePeriod(+item[0].end + 1)}
               </h3>
             </div>
@@ -149,15 +155,21 @@ const DayClasses = ({ currentWeekday }: IDayClasses) => {
                       <span className="dayClasses__info-item-class">
                         {" "}
                         {info.data.whichClass}{" "}
-                      </span>- {info.data.teacher}
+                      </span>
+                      - {info.data.teacher}
                     </h4>
-                    <h4 className="dayClasses__info-item-classroom">{info.data.classroom}</h4>
+                    <h4 className="dayClasses__info-item-classroom">
+                      {info.data.classroom}
+                    </h4>
                     <h5 className="dayClasses__info-item-description">
                       {info.data.classDescription}
                     </h5>
                   </div>
                 ) : (
-                  <div className="dayClasses__info-item-title" key={index}></div>
+                  <div
+                    className="dayClasses__info-item-title"
+                    key={index}
+                  ></div>
                 )
               )}
             </div>
