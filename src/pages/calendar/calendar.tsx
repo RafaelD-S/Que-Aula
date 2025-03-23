b7c18a2faadce3292030f829a24fcc78afa1e329
Luana Lima <luanalima.2006@hotmail.com>
2025-03-22 23:34:00 -0300

    feat: set and style time periods

--- src/pages/calendar/calendar.tsx
+++ src/pages/calendar/calendar.tsx
@@ -1,33 +1,32 @@
 import "./calendar.style.scss";
 import { useEffect, useState } from "react";
-import { IClasses, ISectionArray } from "./calendar.interface";
+import { IClasses } from "./calendar.interface";
 import { IClassesData } from "../../types/dataClasses.interface";
 
 const Calendar = () => {
-  const [day, setDay] = useState<ISectionArray[][]>([]);
   const [classes, setClasses] = useState<IClasses[]>([
     {
       day: "Domingo",
       classes: [],
     },
     {
-      day: "Segunda-Feira",
+      day: "Seg",
       classes: [],
     },
     {
-      day: "Terça-Feira",
+      day: "Ter",
       classes: [],
     },
     {
-      day: "Quarta-Feira",
+      day: "Qua",
       classes: [],
     },
     {
-      day: "Quinta-Feira",
+      day: "Qui",
       classes: [],
     },
     {
-      day: "Sexta-Feira",
+      day: "Sex",
       classes: [],
     },
     {
@@ -53,91 +52,48 @@ const Calendar = () => {
       })
     );
 
-    // console.log(classes);
+    console.log(classes);
   }, []);
 
-  useEffect(() => {
-    classes.forEach((day) => {
-      const updatedSections = day.classes.map((item) => ({
-        start: item.period[0],
-        end: item.period[item.period.length - 1],
-        data: item,
-      }));
+  const definePeriod = (num: number) => {
+    switch (num) {
+      case 0:
+        return "17h";
+      case 1:
+        return "17h50";
+      case 2:
+        return "18h40";
+      case 3:
+        return "19h30";
+      case 4:
+        return "20h20";
+      case 5:
+        return "21h10";
+      case 6:
+        return "22h";
+    }
+  };
 
-      const newArray: ISectionArray[][] = Array(6).fill(null);
+  const setDate = (dia: string) => {
+    if (dia === "Domingo" || dia === "Sábado") return false;
+    else return true;
+  };
 
-      updatedSections.forEach((e) => {
-        for (let i = +e.start; i <= +e.end; i++) {
-          newArray[i] = newArray[i] ? [...newArray[i], e] : [e];
-        }
-      });
-
-      let i = 0;
-      while (i < newArray.length) {
-        if (newArray[i] === null) {
-          const start = i;
-          let end = i;
-
-          while (end < newArray.length && newArray[end] === null) {
-            end++;
-          }
-
-          const emptySection = [
-            {
-              start: start,
-              end: end - 1,
-              data: null,
-            },
-          ];
-
-          for (let j = start; j < end; j++) {
-            newArray[j] = emptySection;
-          }
-
-          i = end;
-        } else {
-          i++;
-        }
-      }
-
-      const uniqueArray: ISectionArray[][] = [];
-      const seen = new Set();
-
-      newArray.forEach((item) => {
-        const key = `${item[0].start}-${item[0].end}-${item[0].data}`;
-
-        if (!seen.has(key)) {
-          seen.add(key);
-          uniqueArray.push(item);
-        }
-      });
-
-      setDay(uniqueArray);
-
-      // console.log(day);
-    });
-  }, [classes]);
-
-  // const definePeriod = (num: number) => {
-  //   switch (num) {
-  //     case 0:
-  //       return "17h";
-  //     case 1:
-  //       return "17h50";
-  //     case 2:
-  //       return "18h40";
-  //     case 3:
-  //       return "19h30";
-  //     case 4:
-  //       return "20h20";
-  //     case 5:
-  //       return "21h10";
-  //     case 6:
-  //       return "22h";
-  //   }
-  // };
-
-  return <main className="calendar">Hello World!</main>;
+  return (
+    <main className="calendar">
+      <h2 className="calendar__title">Todas as Aulas</h2>
+      <article className="calendar__container">
+        <div className="calendar__schedule">
+          {Array.from({ length: 6 }, (_, i) => (
+            <div className="calendar__schedule__item">
+              <h3 key={i}>{definePeriod(i)}</h3>
+              <h3 key={i}>{definePeriod(i + 1)}</h3>
+            </div>
+          ))}
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default Calendar;
