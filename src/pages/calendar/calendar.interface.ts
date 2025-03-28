822574aecb19c5e0bc0e285768cef1b32f731a18
Luana Lima <luanalima.2006@hotmail.com>
2025-03-28 01:52:53 -0300

    style: add truncate text function

--- src/pages/calendar/calendar.interface.ts
+++ src/pages/calendar/calendar.interface.ts
@@ -4,20 +4,3 @@ export interface IClasses {
   day: string;
   classes: IClassesData["classes"];
 }
-
-export interface ISectionArray {
-  start: string | number;
-  end: string | number;
-  data: {
-    weekDay: string;
-    period: string[];
-    teacher: string;
-    selected?: boolean;
-    classList?: string[];
-    classroom: string;
-    whichClass?: string;
-    classDescription?: string;
-    className?: string;
-    greve?: boolean;
-  } | null;
-}
