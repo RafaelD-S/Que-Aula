02063dddaad5baed46a5994d010d4edc384e6532
Andrey <andreygdsn@gmail.com>
2025-05-02 20:48:10 -0300

    feat: add `handleClick` and `handleCheckboxClick` to logic to `classItem` and `mixin.dynamicLineClamp` to style

--- src/components/classItem/classItem.tsx
+++ src/components/classItem/classItem.tsx
@@ -1,15 +1,53 @@
 import "./classItem.style.scss";
 import { IClassItem } from "./classItem.Interface";
+import { useState } from "react";
 
 const ClassItem = ({ data }: { data: IClassItem }) => {
-  const { name, description, semester, line, prerequisites, credit, state } =
-    data;
+  const {
+    name,
+    description,
+    //semester,
+    // line,
+    // prerequisites,
+    credit,
+    state,
+  } = data;
+
+  const [classState, setClassState] = useState(state);
+
+  const handleClick = () => {
+    if (classState === 0) {
+      setClassState(1);
+    }
+    if (classState === 1) {
+      setClassState(0);
+    }
+    if (classState === 2) {
+      setClassState(1);
+    }
+  };
+  const handleCheckboxClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    if (classState === 0) {
+      setClassState(1);
+    }
+    if (classState === 1) {
+      setClassState(2);
+    }
+    if (classState === 2) {
+      setClassState(1);
+    }
+  };
 
   return (
-    <div className={`class-item class-item--state-${state}`}>
+    <div
+      onClick={handleClick}
+      className={`class-item class-item--state-${classState}`}
+    >
       <div className="class-item_checkbox-area">
         <div
-          className={`class-item__checkbox class-item__checkbox--state-${state}`}
+          onClick={handleCheckboxClick}
+          className={`class-item__checkbox class-item__checkbox--state-${classState}`}
         ></div>
       </div>
       <div className="class-item__content">
