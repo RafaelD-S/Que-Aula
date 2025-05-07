b90f57fa5f7133edd8b0e4540e31781b8c3e9d01
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2025-05-07 20:53:44 -0300

    refactor: update structure and classes for better view

--- src/pages/flowchart/flowchart.tsx
+++ src/pages/flowchart/flowchart.tsx
@@ -5,17 +5,20 @@ import { IClassItem } from "../../components/classItem/classItem.Interface";
 import { useCallback, useEffect, useState } from "react";
 
 const Flowchart = () => {
-  const deepEqual = (a: Array<Array<IClassItem>>, b: Array<Array<IClassItem>>): boolean =>
-    JSON.stringify(a) === JSON.stringify(b);
-
   const defaultData: IClassItem[][] = Data.map((semester) =>
-    semester.map((item) => ({
-      name: item.name,
-      description: item.description,
-      prerequisites: item.prerequisites || [],
-      credit: item.credit,
-      state: item.state || "default",
-    }))
+    semester.map((item) => {
+      const { state, credit, requiredFor, description, name } = item;
+
+      if (item.state === "empty") return { state };
+
+      return {
+        name,
+        description,
+        requiredFor,
+        credit,
+        state,
+      };
+    })
   );
 
   const [classData, setClassData] = useState<IClassItem[][]>(() => {
@@ -34,6 +37,29 @@ const Flowchart = () => {
     return defaultData;
   });
 
+  const deepEqual = (a: Array<Array<IClassItem>>, b: Array<Array<IClassItem>>): boolean =>
+    JSON.stringify(a) === JSON.stringify(b);
+
+  const handleClassStateChange = useCallback((itemName: string | undefined, newState: string) => {
+    if (!itemName) return;
+
+    setClassData((prevClassData) => {
+      const newData = prevClassData.map((semester) => {
+        return semester.map((item) => {
+          if (item.name === itemName) {
+            return { ...item, state: newState };
+          }
+          return item;
+        });
+      });
+
+      if (deepEqual(newData, prevClassData)) {
+        return prevClassData;
+      }
+      return newData;
+    });
+  }, []);
+
   useEffect(() => {
     try {
       const currentStorageData = localStorage.getItem("classData");
@@ -45,38 +71,21 @@ const Flowchart = () => {
     }
   }, [classData]);
 
-  const handleClassStateChange = useCallback(
-    (itemName: string | undefined, newState: string) => {
-      if (!itemName) return;
+  useEffect(() => {
+    const teste = classData.map((item) => item);
 
-      setClassData((prevClassData) => {
-        const newData = prevClassData.map((semester) => {
-          return semester.map((item) => {
-            if (item.name === itemName) {
-              return { ...item, state: newState };
-            }
-            return item;
-          });
-        });
-
-        if (deepEqual(newData, prevClassData)) {
-          return prevClassData;
-        }
-        return newData;
-      });
-    },
-    []
-  );
+    console.log(teste);
+  }, []);
 
   return (
     <main className="flowchart">
-      <div>
-        <h2 className="flowchart__title">Fluxograma</h2>
-        <article className="flowchart__container">
+      <h2 className="flowchart__title">Fluxograma</h2>
+      <article className="flowchart__container">
+        <div className="flowchart__container__content">
           {classData.map((semester, index) => (
             <div key={index} className="flowchart__semester">
               <h3 className="flowchart__semester-title">{index + 1}º Semestre</h3>
-              <div className="flowchart__semester-content">
+              <div className="flowchart__semester-classes">
                 {semester.map((item, line) => (
                   <ClassItem
                     key={`${index}-${line}`}
@@ -87,8 +96,8 @@ const Flowchart = () => {
               </div>
             </div>
           ))}
-        </article>
-      </div>
+        </div>
+      </article>
     </main>
   );
 };
